6968a3ce68901c1cfb9dbe68d45dd6222883513d
JoseGhu <69773420+JoseGhu@users.noreply.github.com>
2021-07-12 21:04:26 -0300

    Feito por Upload

---
+++ src/components/template/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+<footer className="footer">
+    <span>
+        Desenvolvido <i className="fa fa-heart text-danger"></i> por
+        <strong> Ghu<span className="text-danger">Sites</span></strong>
+    </span>
+</footer>
